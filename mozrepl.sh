7b8f110b93e73dc71a5c77af7a98e357711857c6
Phil Hudson <phil.hudson@iname.com>
2013-05-20 15:11:34 +0100

    Small refactorings.

--- mozrepl.sh
+++ mozrepl.sh
@@ -9,6 +9,37 @@
 
 ## Short French-English lexicon at end
 
+
+# Enable (1) or disable (0) diagnostic output
+moz__DEBUG=0
+
+moz__mktemp_gnu () { # $1: mktemp binary name (mktemp or gmktemp) $2: template
+    "$1" --tmpdir "${2}.XXX"
+}
+
+moz__mktemp_bsd () { # $1: template
+    mktemp -t "$1"
+}
+
+moz__mktemp () { # $1: template
+    case "$OSTYPE" in
+
+        [Dd]arwin*|*BSD)
+            # Do we have the GNU mktemp installed anywhere on PATH?
+            if [ -x "`which gmktemp`" ] ; then
+                moz__mktemp_gnu gmktemp $@
+            else
+                moz__mktemp_bsd $@
+            fi
+            ;;
+
+        *)
+            moz__mktemp_gnu mktemp $@
+            ;;
+
+    esac
+}
+
 moz_cmd () { # $1: commande javascript
 expect <<EOF
     spawn  nc localhost 4242
@@ -44,24 +75,26 @@ moz_waitURL () {  # $1: URL
 
 
 moz_findLinks () { # $1: texte du lien
-
-cat > /tmp/findLinks.js <<EOF
+    local FIND_LINKS="`moz__mktemp findLinks`"
+    (( ${moz__DEBUG:-0} )) && echo "FIND_LINKS: $FIND_LINKS" >&2
+cat > "$FIND_LINKS" <<EOF
 
     var exp = new RegExp("$1");
 
     for ( var i=0; i<document.links.length; i++ ) {
-        if ( exp.test( document.links[i].text ) )     repl.print(content.location.href)"
+        if ( exp.test( document.links[i].text ) )     // repl.print(content.location.href)"
             repl.print( document.links[i].href );
     }
 
 EOF
-    moz_script file:///tmp/findLinks.js | grep "^file\|^http" | sed -e 's/\r//g'
+    (( ${moz__DEBUG:-0} )) && echo Wrote file >& 2
+    moz_script "file://${FIND_LINKS}" | sed '/^file|^http/ s/\r//g'
 }
 
 
 moz_setInput () { # $1: nom  $2: valeur
-
-cat > /tmp/setInput.js <<EOF
+    local SET_INPUT="`moz__mktemp setInput`"
+cat > "$SET_INPUT" <<EOF
 
     var c = this.document.getElementsByTagName("input");  // [object HTMLCollection]
                                                           // [object HTMLInputElement] .type=text .name=q .value=Shakira
@@ -73,13 +106,13 @@ cat > /tmp/setInput.js <<EOF
     }
 
 EOF
-    moz_script file:///tmp/setInput.js
+    moz_script "file://${SET_INPUT}"
 }
 
 
 moz_submitFormByName () { # $1: nom formulaire
-
-cat > /tmp/submitForm.js <<EOF
+    local SUBMIT_FORM="`moz__mktemp submitForm`"
+cat > "$SUBMIT_FORM" <<EOF
 
     var c = this.document.getElementsByTagName("form");
 
@@ -88,14 +121,13 @@ cat > /tmp/submitForm.js <<EOF
             c[i].submit();
     }
 EOF
-    moz_script file:///tmp/submitForm.js
-
+    moz_script "file://${SUBMIT_FORM}"
 }
 
 
 moz_setTextArea () { # $1: nom  $2: valeur
-
-cat > /tmp/setTextArea.js <<EOF
+    local SET_TEXT_AREA="`moz__mktemp setTextArea`"
+cat > "$SET_TEXT_AREA" <<EOF
 
     var c = this.document.getElementsByTagName("textarea");  // [object HTMLCollection]
                                                              // [object HTMLTextAreaElement] type name value
@@ -104,13 +136,13 @@ cat > /tmp/setTextArea.js <<EOF
             c[i].value = "$2";
 
 EOF
-    moz_script file:///tmp/setTextArea.js
+    moz_script "file://${SET_TEXT_AREA}"
 }
 
 
 moz_setSelect () { # $1: nom $2: valeur
-
-cat > /tmp/setSelect.js <<EOF
+    local SET_SELECT="`moz__mktemp setSelect`"
+cat > "$SET_SELECT" <<EOF
 
     var c = this.document.getElementsByTagName("select");  // [object HTMLCollection]
                                                            // [object HTMLSelectElement]
@@ -119,13 +151,13 @@ cat > /tmp/setSelect.js <<EOF
             c[i].value = "$2";
 
 EOF
-    moz_script file:///tmp/setSelect.js
+    moz_script "file://${SET_SELECT}"
 }
 
 
 moz_setInputRadio () { # $1: nom $2: valeur
-
-cat > /tmp/setInputRadio.js <<EOF
+    local SET_INPUT_RADIO="`moz__mktemp setInputRadio`"
+cat > "$SET_INPUT_RADIO" <<EOF
 
     var c = this.document.getElementsByTagName("input");  // [object HTMLCollection]
 
@@ -136,7 +168,7 @@ cat > /tmp/setInputRadio.js <<EOF
 
     }
 EOF
-    moz_script file:///tmp/setInputRadio.js
+    moz_script "file://${SET_INPUT_RADIO}"
 }
 
 
@@ -151,8 +183,8 @@ moz_waitTitle () {  # $1: URL
 
 
 moz_listSelectValues () { # $1: nom
-
-cat > /tmp/listSelectValues.js <<EOF
+    local LIST_SELECT_VALUES="`moz__mktemp listSelectValues`"
+cat > "$LIST_SELECT_VALUES" <<EOF
 
     var c = this.document.getElementsByTagName("select");  // [object HTMLCollection]
                                                            // [object HTMLSelectElement]
@@ -167,13 +199,14 @@ cat > /tmp/listSelectValues.js <<EOF
         }
 
 EOF
-    moz_script file:///tmp/listSelectValues.js | grep "^VALUE  " | sed 's/^VALUE\t//; s/\r//g; /^\s*$/d'
+    moz_script "file://${LIST_SELECT_VALUES}" | \
+    sed '/^VALUE\s+/ s/^VALUE\t//; s/\r//g; /^\s*$/d'
 }
 
 
 moz_getInput () { # $1: nom
-
-cat > /tmp/getInput.js <<EOF
+    local GET_INPUT="`moz__mktemp getInput`"
+cat > "$GET_INPUT" <<EOF
 
     var c = this.document.getElementsByTagName("input");  // [object HTMLCollection]
                                                           // [object HTMLInputElement] .type=text .name=q .value=Shakira
@@ -185,13 +218,14 @@ cat > /tmp/getInput.js <<EOF
     }
 
 EOF
-    moz_script file:///tmp/getInput.js | grep "^VALUE	" | sed 's/^VALUE\t//; s/\r//g; /^\s*$/d'
+    moz_script "file://${GET_INPUT}" | \
+    sed '/^VALUE\s+/ s/^VALUE\t//; s/\r//g; /^\s*$/d'
 }
 
 
 moz_setInputCheckbox () { # $1: nom $2: valeur
-
-cat > /tmp/setInputCheckbox.js <<EOF
+    local SET_INPUT_CHECKBOX="`moz__mktemp setInputCheckbox`"
+cat > "$SET_INPUT_CHECKBOX" <<EOF
 
     var c = this.document.getElementsByTagName("input");  // [object HTMLCollection]
 
@@ -202,7 +236,7 @@ cat > /tmp/setInputCheckbox.js <<EOF
 
     }
 EOF
-    moz_script file:///tmp/setInputCheckbox.js
+    moz_script "file://${SET_INPUT_CHECKBOX}"
 }
 
 
